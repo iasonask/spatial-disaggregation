e8a0bf8cbb1f31d504fa082c4b07cbdeb2e4728f
Iasonas Kouveliotis-Lysikatos <iasonas@kth.se>
2020-10-13 15:49:15 +0200

    Opimize only branch paramters

--- Optimisation 1.py
+++ Optimisation 1.py
@@ -20,29 +20,28 @@ t220 = 7e-2
 t300 = 4e-2
 t380 = 2.8e-2
 
-x0 = [Z380, Z300, Z220, t380, t300, t220]
+# x0 = [Z380, Z300, Z220, t380, t300, t220]
+x0 = [Z380, Z300, Z220]
 
 
 def objective(x):
     ohm_per_km = [x[0], x[1], x[2]]
-    trafo_x = [x[3], x[4], x[5]]
+    # trafo_x = [x[3], x[4], x[5]]
     m = N490(year=2018)
-    m.branch_params(ohm_per_km, trafo_x)
-    # Make sure to modify the function to accept these parameters along with function call
+    m.branch_params(ohm_per_km)
     m.time_series('20180101:00', '20180102:00')    
     err = m.calculate_errors()
     print(x, err['MAE'].sum())
-
     return err['MAE'].sum()
-
 
 bz1 = (0.2, 0.6)
 bz2 = (0.2, 0.6)
 bz3 = (0.2, 0.6)
-bt1 = (2.8e-2, 1.5e-1)
-bt2 = (4e-2, 1.5e-1)
-bt3 = (7e-2, 1.5e-1)
-bounds = (bz1, bz2, bz3, bt1, bt2, bt3)
+# bt1 = (2.8e-2, 1.5e-1)
+# bt2 = (4e-2, 1.5e-1)
+# bt3 = (7e-2, 1.5e-1)
+# bounds = (bz1, bz2, bz3, bt1, bt2, bt3)
+bounds = (bz1, bz2, bz3)
 
 sol = minimize(objective, x0, method='SLSQP', bounds=bounds, options={'disp': True, 'eps': 1e-3})
 print(sol)
